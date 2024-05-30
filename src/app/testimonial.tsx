a04100e031b6cdeec9a2729b5f395bf0c80f88f0
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2024-05-30 16:17:36 +0800

    Update testimonial.tsx

--- src/app/testimonial.tsx
+++ src/app/testimonial.tsx
@@ -25,13 +25,6 @@ export function Testimonial() {
         <Card color="transparent" shadow={false} className="py-8 lg:flex-row">
           <CardBody className="w-full lg:gap-10 h-full lg:!flex justify-between ">
             <div className="w-full mb-10 lg:mb-0">
-              <Typography
-                variant="h3"
-                color="blue-gray"
-                className="mb-4 font-bold lg:max-w-xs"
-              >
-                Mobile App Development
-              </Typography>
               <Typography className="mb-3 w-full lg:w-8/12 font-normal !text-gray-500">
                 I had the pleasure of working with Lily on a critical web
                 development project, and I can confidently say that their
@@ -44,51 +37,40 @@ export function Testimonial() {
                 variant="small"
                 className="font-normal mb-5 !text-gray-500"
               >
-                Marketing @ APPLE INC.
+                Lane Woods
               </Typography>
-              <div className="flex items-center gap-4">
-                <Avatar
-                  variant="rounded"
-                  src="/image/avatar1.jpg"
-                  alt="spotify"
-                  size="sm"
-                  className={`cursor-pointer ${
-                    active === 1 ? "opacity-100" : "opacity-50"
-                  }`}
-                  onClick={() => setActive(1)}
-                />
-                <div className="w-[1px] h-[36px] bg-blue-gray-100 "></div>
-                <Avatar
-                  variant="rounded"
-                  src="/image/avatar2.jpg"
-                  alt="spotify"
-                  size="sm"
-                  className={`cursor-pointer ${
-                    active === 2 ? "opacity-100" : "opacity-50"
-                  }`}
-                  onClick={() => setActive(2)}
-                />
-                <div className="w-[1px] h-[36px] bg-blue-gray-100" />
-                <Avatar
-                  variant="rounded"
-                  src="/image/avatar3.jpg"
-                  alt="spotify"
-                  size="sm"
-                  className={`cursor-pointer ${
-                    active === 3 ? "opacity-100" : "opacity-50"
-                  }`}
-                  onClick={() => setActive(3)}
-                />
-              </div>
             </div>
-            <div className="h-[21rem] rounded-lg w-full sm:w-[18rem] shrink-0">
-              <Image
-                width={768}
-                height={768}
-                alt="testimonial image"
-                src={`/image/avatar${active}.jpg`}
-                className="h-full rounded-lg w-full object-cover"
-              />
+            <div className="w-full mb-10 lg:mb-0">
+              <Typography className="mb-3 w-full lg:w-8/12 font-normal !text-gray-500">
+                I had the pleasure of working with Lily on a critical web
+                development project, and I can confidently say that their
+                expertise and professionalism exceeded my expectations.
+              </Typography>
+              <Typography variant="h6" color="blue-gray" className="mb-0.5">
+                Michael - Technical Manager
+              </Typography>
+              <Typography
+                variant="small"
+                className="font-normal mb-5 !text-gray-500"
+              >
+                Ziio
+              </Typography>
+            </div>
+            <div className="w-full mb-10 lg:mb-0">
+              <Typography className="mb-3 w-full lg:w-8/12 font-normal !text-gray-500">
+                I had the pleasure of working with Lily on a critical web
+                development project, and I can confidently say that their
+                expertise and professionalism exceeded my expectations.
+              </Typography>
+              <Typography variant="h6" color="blue-gray" className="mb-0.5">
+                Michael - Technical Manager
+              </Typography>
+              <Typography
+                variant="small"
+                className="font-normal mb-5 !text-gray-500"
+              >
+                Trifairy
+              </Typography>
             </div>
           </CardBody>
         </Card>
