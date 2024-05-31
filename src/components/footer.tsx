2c147a5875b3e762cfce92690fc148413c2a35ab
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2024-05-31 17:39:35 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,15 +9,19 @@ export function Footer() {
       <div className="container mx-auto">
         <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 border-t border-gray-200 py-6 md:justify-between">
           <Typography className="text-center font-normal !text-gray-700">
-            &copy; {CURRENT_YEAR} Made with{" "}
-            <a href="https://www.material-tailwind.com" target="_blank">
-              Material Tailwind
+            &copy; {CURRENT_YEAR} | {" "}
+            <a href="https://velicium.xyz" target="_blank">
+              Royale Accessories
             </a>{" "}
-            by{" "}
-            <a href="https://www.creative-tim.com" target="_blank">
-              Creative Tim
+
+          {/*             
+            maintained by{" "}
+            <a href="https://velicium.xyz" target="_blank">
+              Velicium Digital
             </a>
-            .
+            . 
+          */}
+
           </Typography>
           <ul className="flex gap-8 items-center">
             {LINKS.map((link) => (
