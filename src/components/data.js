547d669cd16d3762b0ea4e05e18c77d1235eddc7
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2024-06-06 10:53:24 +0800

    Add icons

--- src/components/data.js
+++ src/components/data.js
@@ -1,11 +1,17 @@
 import {
-  FaceSmileIcon,
-  ChartBarSquareIcon,
-  CursorArrowRaysIcon,
-  DevicePhoneMobileIcon,
-  AdjustmentsHorizontalIcon,
-  SunIcon,
-} from "@heroicons/react/24/solid";
+  bangles,
+  bracelet,
+  brooche,
+  earrings,
+  necklace,
+  rings,
+  design,
+  threedimensions,
+  manufacturing,
+  goldplating,
+  vacuumchamber,
+  qa
+} from "../../public/img/icons";
 
 import benefitOneImg from "../../public/img/benefit-one.png";
 import benefitTwoImg from "../../public/img/benefit-two.png";
@@ -18,32 +24,32 @@ const benefitOne = {
     {
       title: "Designing",
       desc: "We have a team of some of the most well-versed, highly experienced creative minds in the industry.",
-      icon: <FaceSmileIcon />,
+      icon: design,
     },
     {
       title: "3D Modeling",
       desc: "Creating beautiful and captivating high quality 3D models of jewelries that satisfy our customers&apos;' requirements.",
-      icon: <ChartBarSquareIcon />,
+      icon: threedimensions,
     },
     {
       title: "Manufacturing",
       desc: "We specialize in manufacturing beautiful custom designs to satisfy our customer's specifications based on their own designs or with the help of our design teams to accurately satisfy our customers.",
-      icon: <CursorArrowRaysIcon />,
+      icon: manufacturing,
     },
     {
       title: "Water Plating",
       desc: "Performance matters. I optimize websites and apps for speed, ensuring your users enjoy a fast and responsive experience, which in turn boosts user satisfaction and SEO rankings.",
-      icon: <FaceSmileIcon />,
+      icon: goldplating,
     },
     {
       title: "Vacume Metalization",
       desc: "My development goes hand-in-hand with an eye for design. I create user interfaces that are not only functional but also aesthetically pleasing, providing a seamless and enjoyable user journey.",
-      icon: <ChartBarSquareIcon />,
+      icon: vacuumchamber,
     },
     {
       title: "Quality Assurance and Testing",
       desc: "We rigorously test our products to guarantee a the best quality assurance for our clients. We do tensile tests, drop tests, sensory tests.",
-      icon: <CursorArrowRaysIcon />,
+      icon: qa,
     },
   ],
 };
@@ -56,32 +62,32 @@ const benefitTwo = {
     {
       title: "Rings",
       desc: "We can craft any design you want with cubic zirconium, 925 sterling silver, brass, zinc alloy, or metal.",
-      icon: <AdjustmentsHorizontalIcon />,
+      icon: rings,
     },
     {
       title: "Bangles",
       desc: "We can craft any design you want with cubic zirconium, 925 sterling silver, brass, zinc alloy, or metal.",
-      icon: <SunIcon />,
+      icon: bangles,
     },
     {
       title: "Earrings",
       desc: "We can craft any design you want with cubic zirconium, 925 sterling silver, brass, zinc alloy, or metal.",
-      icon: <AdjustmentsHorizontalIcon />,
+      icon: earrings,
     },
     {
       title: "Bracelets",
       desc: "We can craft any design you want with cubic zirconium, 925 sterling silver, brass, zinc alloy, or metal.",
-      icon: <SunIcon />,
+      icon: bracelet,
     },
     {
       title: "Brooches",
       desc: "We can craft any design you want with cubic zirconium, 925 sterling silver, brass, zinc alloy, or metal.",
-      icon: <AdjustmentsHorizontalIcon />,
+      icon: brooche,
     },
     {
       title: "Necklaces",
       desc: "We can craft any design you want with cubic zirconium, 925 sterling silver, brass, zinc alloy, or metal.",
-      icon: <SunIcon />,
+      icon: necklace,
     },
   ],
 };
