f60b64366f3c7970aefe9b55b96dfefae80a1c0a
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2024-06-06 11:40:44 +0800

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Container } from "@/components/Container";
-import heroImg from "../../public/img/hero.png";
+import heroImg from "../../public/img/hero.jpg";
 
 export const Hero = () => {
   return (
@@ -24,17 +24,9 @@ export const Hero = () => {
                   href="/Royale_Brochure.pdf"
                   target="_blank"
                   rel="noopener"
+                  download={true}
                   className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
-                  Download Our Brochure
-                </a>
-              </div>
-              <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-                <a
-                  href="/Royale_Brochure.pdf"
-                  target="_blank"
-                  rel="noopener"
-                  className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
-                  Download Company Profile
+                  Download Brochure
                 </a>
               </div> 
             </div>
